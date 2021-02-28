3955cc0125a921d2fb58172be82cc4a09b8ab282
Erik von Brömssen <erik.von.b@hotmail.com>
2021-02-28 01:29:58 +0100

    std alg in correctness test

--- cref_test.jl
+++ cref_test.jl
@@ -2,21 +2,49 @@ using Nemo
 using DelimitedFiles
 using Printf
 
-
-function getlows(m)
-  lows = zeros(Int, size(m,2))
-  for j = 1:size(m,2)
-    l = findlast(!iszero, m[:,j])
-    if (isnothing(l))
-      continue
+function columnlow(c)
+  for i in size(c, 1) : -1 : 1
+    if (c[i] != 0)
+      return i
     end
-    lows[j] = l
+  end
+  return nothing
+end
+
+function getlows(d)
+  lows = zeros(Int, size(d, 2))
+  for j in 1:size(d, 2)
+    lowj = columnlow(d[:,j])
+    lows[j] = lowj == nothing ? 0 : lowj
   end
   return lows
 end
 
+function stdalg(d)
+  for j in 2 : size(d, 2)
+    isreduced = false
+
+    while !isreduced
+      # Assume d_j is reduced
+      isreduced = true
+      
+      for k in 1:(j - 1)
+        lowj = columnlow(d[:,j])
+        lowk = columnlow(d[:,k])
+        if lowj != nothing && lowj == lowk
+          d[:,j] += d[:,k]
+          # If d_j wasn't reduced, set false
+          isreduced = false
+          break
+        end
+      end
+    end
+  end  
+  return d
+end
+
 function isinjective(v)
-  for i = 1:(length(v) - 1)
+  for i = 1 : (length(v) - 1)
     if v[i] > 0 && v[i] in v[i+1:end]
       return false
     end
@@ -50,31 +78,39 @@ function main()
 
     run(`./main $filename_in $filename_out`)
 
-    d_original = readdlm(filename_in,  ' ', Int, '\n')
-    d_reduced  = readdlm(filename_out, ' ', Int, '\n')
-    n = size(d_original, 1)
+    println("reading original")
+    d_original´ = readdlm(filename_in,  ' ', Int, '\n')
+    println("reading reduced")
+    d_reduced´ = readdlm(filename_out, ' ', Int, '\n')
+    n = size(d_original´, 1)
 
     Z_2, _ = FiniteField(2, 2, "x")
     S = MatrixSpace(Z_2, n, n)
 
+    d_original = S(d_original´)
+    d_reduced = S(d_reduced´)
+    d_true_reduced = stdalg(d_original)
+
     # Transpose to find column-reduced echelon form instead of row-reduced
-    _, cref_original = rref(S(d_original'))
-    _, cref_reduced  = rref(S(d_reduced'))
+    _, cref_original = rref(d_original')
+    _, cref_reduced  = rref(d_reduced')
 
-    # println("CREF of original matrix is:")
-    # display(cref_original')
-    # println("")
+    if !isinjective(getlows(d_reduced))
+      println("FAILED TEST: low(⋅) not injective")
+      return
 
-    # println("CREF of reduced matrix is:")
-    # display(cref_reduced')
-    # println("")
+    elseif getlows(d_reduced) != getlows(d_true_reduced)
+      println("FAILED TEST: low(⋅) does not match std alg. Std alg gives the matrix:")
+      println(d_true_reduced)
+      return
 
-    if isinjective(getlows(d_original)) && cref_original == cref_reduced
+    elseif cref_original != cref_reduced
+      println("FAILED TEST: crefs not identical")
+      return
+      
+    else
       println("PASSED TEST")
       passedcount += 1
-    else
-      println("FAILED TEST")
-      return
     end
   end
 
